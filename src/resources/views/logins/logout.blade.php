ff6902f7a1c3ee719797fb296a5b12bee325e7aa
hoku23 <hoku153509@gmail.com>
2022-06-29 21:45:38 +0900

    logoutView commit

---
+++ src/resources/views/logins/logout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>QLife</title>
+        <meta name="description" content="暮らしに関する情報をシェアするアプリケーションです">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width initial-scale=1.0">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/phone_style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/mini_pc_style.css') }}">
+        <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon" />
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="{{ asset('images/logo.png') }}" alt="ロゴ">
+            </div>
+            <div id="to-registrationInput">
+                <a href="{{ route('logins.index') }}"><button class="toRegistration-btn">ログイン</button></a>
+            </div>
+        </header>
+        <main>
+            <article>
+                <div class="login">
+                    <h1>ログアウトしました</h1>
+                    
+                </div>
+            </article>
+        </main>
+
+        <script src="index.js"></script>
+    </body>
+</html>
